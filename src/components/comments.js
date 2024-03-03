12e47ef4e03d587c10948490babbecaffe711d60
megha8191 <meghachhabra819@gmail.com>
2024-03-03 06:03:23 +0530

    top level comments added

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,74 +2,85 @@
 import React from 'react'
 
 
-const commentsList = [
-  { 
-    text:"Lorem ipsum dolor sit amet, consectetur adip",
-    username: 'megha',
-    reply: [
-      { 
-        text:"Lorem ipsum dolor sit amet, consectetur adip",
-        username: 'megha',
-        reply: [
-          { 
-            text:"Lorem ipsum dolor sit amet, consectetur adip",
-            username: 'megha',
-            reply: [
-              { 
-                text:"Lorem ipsum dolor sit amet, consectetur adip",
-                username: 'megha',
-                reply: [
-                  { 
-                    text:"Lorem ipsum dolor sit amet, consectetur adip",
-                    username: 'megha',
-                    reply: [],
-                  },
-                  { 
-                    text:"Lorem ipsum dolor sit amet, consectetur adip",
-                    username: 'megha',
-                    reply: [],
-                  },
-                  { 
-                    text:"Lorem ipsum dolor sit amet, consectetur adip",
-                    username: 'megha',
-                    reply: [],
-                  },
-                  { 
-                    text:"Lorem ipsum dolor sit amet, consectetur adip",
-                    username: 'megha',
-                    reply: [],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      { 
-        text:"Lorem ipsum dolor sit amet, consectetur adip",
-        username: 'megha',
-        reply: [],
-      },
-    ],
-  },
-  { 
-    text:"Lorem ipsum dolor sit amet, consectetur adip",
-    username: 'megha',
-    reply: [],
-  },
-];
+// const commentsList = [
+//   { 
+//     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//     username: 'megha',
+//     reply: [
+//       { 
+//         text:"Lorem ipsum dolor sit amet, consectetur adip",
+//         username: 'megha',
+//         reply: [
+//           { 
+//             text:"Lorem ipsum dolor sit amet, consectetur adip",
+//             username: 'megha',
+//             reply: [
+//               { 
+//                 text:"Lorem ipsum dolor sit amet, consectetur adip",
+//                 username: 'megha',
+//                 reply: [
+//                   { 
+//                     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//                     username: 'megha',
+//                     reply: [],
+//                   },
+//                   { 
+//                     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//                     username: 'megha',
+//                     reply: [],
+//                   },
+//                   { 
+//                     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//                     username: 'megha',
+//                     reply: [],
+//                   },
+//                   { 
+//                     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//                     username: 'megha',
+//                     reply: [],
+//                   },
+//                 ],
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//       { 
+//         text:"Lorem ipsum dolor sit amet, consectetur adip",
+//         username: 'megha',
+//         reply: [],
+//       },
+//     ],
+//   },
+//   { 
+//     text:"Lorem ipsum dolor sit amet, consectetur adip",
+//     username: 'megha',
+//     reply: [],
+//   },
+// ];
 
-console.log(commentsList[1])
 
-const Commentdiv = (info)=>{
-  let {username,text,reply}= info.info;
+const Comments = (item)=>{
+  console.log(item)
+  const replies = item.info.replies;
+
+  const com = item.info.snippet.topLevelComment.snippet;
+  const {authorDisplayName,authorProfileImageUrl,textOriginal}= com;
+  console.log(item.info)
   return(
     <div className='commentdiv mb-3'>
-      <b>{username}</b>
-      <p>{text}</p>
-      {(reply.length)===0? "":
-         reply.map(function(reply, index){
-          return ( <div className='border-l-gray-700 border-l pl-5' key={index}>
+      <div class="w-100 flex pt-3 gap-3 mt-3">
+            <img src={authorProfileImageUrl} class="basis-auto bg-gray-400 rounded-full w-9 h-9" alt="profle-img"/>
+            <div class=" basis-5/6 col-span-11 ">
+                <p class="font-semibold font-xs mb-0 text-black">{authorDisplayName}</p>
+                <p class="text-gray-500 font-xs mt-0">{textOriginal}</p>
+                <p className='text-blue-700'>{item.info.snippet.totalReplyCount} Replies</p>
+            </div>
+        </div>
+      {(item.info.snippet.totalReplyCount)<=0? "":
+        // <p>jsosi{item.info.replies.comments.length}</p>
+         item.info.replies.comments.map(function(reply, index){
+          return ( <div className='border-l-gray-700 border-l pl-6' key={index}>
            <Commentdiv info={reply}/>
            </div>
            )}
@@ -79,14 +90,12 @@ const Commentdiv = (info)=>{
   )
 }
 
-const Comments = () => {
+const Commentdiv = () => {
   return (
     <div className='mt-5'>
       <b className='text-xl font-bold'>Comments</b>
       <div className=''>
-       { commentsList.map(function(comment,index){
-          return (<Commentdiv info ={comment} key={index} />)
-        })}
+     
       </div>
     </div>
   )
--- src/components/videoDetail.js
+++ src/components/videoDetail.js
@@ -20,30 +20,55 @@ const Videodetail = () => {
         getComments();
     }, [])
 
-    async function getComments(){
+    async function getComments() {
         const comments = await fetch(" https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies%2Cid&maxResults=50&videoId=" + videoId + "&key=" + google_api_key)
         const commentData = await comments.json();
         setCommentList(commentData.items);
         console.log(commentsList)
     }
-    
-    return (        
+
+    return (
         <div className='w-100 basis-full pr-3'>
             <div className='basis-full flex'>
                 <div className='basis-4/6'>
                     <iframe width="950" style={{ maxWidth: "100%" }} height="500" src={"https://www.youtube.com/embed/" + videoId} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     <div className='Comments'>
-                <Comments/>
-            </div>
+                        {(commentsList)? (
+                            commentsList.map(function(item){
+                                return (<Comments info ={item} key={item.id} />)
+                            })
+                        ):"loading"}
+                    </div>
                 </div>
                 <div className='basis-2/6 pl-3 pr-3'>
-                    <LiveChat/>
+                    <LiveChat />
+                    {
+                        // 
+                        <a className=" grid-cols-[minmax(auto,210px),1fr,auto] sm:grid w-full gap-2 mt-4" href="/watch?v=jwYRwOj9RdA">
+                            <div className="relative w-full">
+                                <img className="w-full rounded-lg object-cover4 h-38 " src="https://i.ytimg.com/vi/jwYRwOj9RdA/mqdefault.jpg" alt="thumbnail" />
+                            </div>
+                            <div className="pb-1 pt-0">
+                                <h3 className="font-medium text-lg line-clamp-2">OKE GAS PRABOWO GIBRAN PALING PAS</h3>
+                                <div className="w-100 flex mt-2 mb-2 gap-2 items-center">
+                                    <span className="basis-auto bg-gray-400 rounded-full w-7 h-7"></span>
+                                    <div className=" basis-5/6 col-span-11 ">
+                                        <p className="text-gray-500 font-xs">Richard Jersey</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="basis-1 pt-1 md:block hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"></path></svg>
+                            </div>
+                        </a>
+                        //  
+                    }
                 </div>
             </div>
         </div>
 
     )
- 
+
 }
 
 export default Videodetail
