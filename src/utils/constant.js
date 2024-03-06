7b0fa66cce8836af3db8a49027c5b6b7cfb05b99
megha8191 <meghachhabra819@gmail.com>
2024-03-07 05:10:02 +0530

    vercel config corsproxy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,6 @@ export const google_api_key="AIzaSyDaYOvtKdmH5SKCKnQZ4uBzuJKGmvzu0jE"
 
 export const youtube_api = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=0&key="+google_api_key;
 
-export const search_autocomplete_api = 
-"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const search_autocomplete_api = 'https://corsproxy.io/?' + encodeURIComponent("https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=");
 
 // export const commentsList = "https://youtube.googleapis.com/youtube/v3/comments?key=" + google_api_key;
