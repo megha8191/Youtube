e7eb4e0cfbeb88ccaafb0bee9d2c5b3e3706fcd3
megha8191 <meghachhabra819@gmail.com>
2024-05-16 19:21:19 +0530

    removed history , subscriptions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,7 +18,7 @@ const Sidebar = () => {
             </svg>
             {(!nav) ? "" : <span className="nav-list-name"> Home</span>}
           </li>
-          <li>
+          {/* <li>
             <Link to="/history">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -33,7 +33,7 @@ const Sidebar = () => {
               </svg>
               {(!nav) ? "" : <span className="nav-list-name"> Subscriptions</span>}
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link to="/live">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
